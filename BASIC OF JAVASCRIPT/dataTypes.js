abb1b96c064958fddcb94991f40bd1d4d0b856f6
Bikash61 <bashyalb96@gmail.com>
2024-03-08 23:06:39 +0545

    Datatypes

---
+++ BASIC OF JAVASCRIPT/dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all JS code as a newer script
+//alert("Hello") we are using node js not browser 
+
+let name = "BIkash"
+let age = 18
+let isLoggedIn = false
+let state
+
+// number => 2 to the power of 53 
+// bigInt 
+// string => " "
+//boolean => true/false 
+//null  => standalone value 
+//undefined => 
+//symbol => unique 
+
+//object 
+console.log(typeof undefined); // undefined 
+console.log(typeof null); //object
