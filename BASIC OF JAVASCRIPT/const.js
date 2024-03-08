1c4b6f298b4ea48d14063218209aecfbb132f47d
Bikash61 <bashyalb96@gmail.com>
2024-03-08 22:45:40 +0545

    Varibales

---
+++ BASIC OF JAVASCRIPT/const.js
@@ -0,0 +1,17 @@
+const accountId = 12332
+let accountEmail = "bashyal31@google.com"
+var accountPassword = "1213"
+accountCity = "Haryana"
+let accountState ;
+
+// accountId = 2332 not allowed
+accountEmail = "bik@google.com"
+accountPassword = "3424"
+accountCity = "Gujrat"
+
+// console.log(accountId);
+// console.log(accountEmail);
+// console.log(accountPassword);
+// console.log(accountCity);
+
+console.table([ accountEmail , accountId, accountPassword, accountCity, accountState])
