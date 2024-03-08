7881e26aa7714c359c6be03273428fcbfcbee184
Bikash61 <bashyalb96@gmail.com>
2024-03-08 23:47:29 +0545

    Conversion of data

---
+++ BASIC OF JAVASCRIPT/conversionAndOperations.js
@@ -0,0 +1,21 @@
+let score = 52.098
+
+// console.log(typeof score )
+
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abs" => NaN
+// true => 1 and false => 0
+
+let isLoggedIn = ''
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
