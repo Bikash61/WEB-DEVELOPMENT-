aa4530a87258832f74e1196194a5109a489b4e7c
Bikash61 <bashyalb96@gmail.com>
2024-03-10 02:05:56 +0545

    Type conversion

---
+++ 01 BASIC/TYPE CONVERSION/question1.js
@@ -0,0 +1,15 @@
+
+// WAP to convert "123" into a number
+let nam = "123"
+let changeData = Number(nam)
+console.log(typeof(changeData))
+
+// write a program to convert 5 into a string 
+let numb = 5
+let changeNumb = String(numb)
+console.log(typeof(changeNumb))
+
+// write a program to convert a boolean into a number '
+let bolNum = true 
+let changeBol = Number(bolNum)
+console.log(typeof(changeBol))
